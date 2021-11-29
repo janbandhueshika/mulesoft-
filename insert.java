41631a25266c88f4c965672552e47fd5f7bd7075
janbandhueshika <81458831+janbandhueshika@users.noreply.github.com>
2021-11-29 22:10:34 +0530

    Add files via upload

---
+++ insert.java
@@ -0,0 +1,41 @@
+package net.sqlitetutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertApp {
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/Movies.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO Movies(name,actor,actress,director,year) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        InsertApp app = new InsertApp();
+        app.insert("Black Widow","Scarlett Johanson","Florence pugh","Cate Shortland",2021);
+        app.insert("Shang-chi and the legend of the ten rings","Simu liu","Awkwafina","Destin Daniel Cretton",2021);
+        app.insert("The avengers", "Robert Downey Jr.","Scarlett Johanson","Joss Whedon", 2012);
+    }
+
+}
